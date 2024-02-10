419f86b2a13f146224fd4db05574bcdb5f3f73a8
voidProc <voidproc@gmail.com>
2024-02-10 18:37:36 +0900

    version

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-pyinstaller --noconfirm --clean --log-level=WARN --workpath=_build --specpath=_spec --name=bms-table-view_v1.1.3 main.py
-copy README.txt dist\bms-table-view_v1.1.3
-copy config.example.json dist\bms-table-view_v1.1.3
+pyinstaller --noconfirm --clean --log-level=WARN --workpath=_build --specpath=_spec --name=bms-table-view_v1.2.0 main.py
+copy README.txt dist\bms-table-view_v1.2.0
+copy config.example.json dist\bms-table-view_v1.2.0

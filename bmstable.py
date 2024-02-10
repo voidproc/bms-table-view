fb63e68b7ac10a76cbf82267e1c6f10609757709
voidProc <voidproc@gmail.com>
2024-02-10 14:23:54 +0900

    フォルダ名変更

--- bmstable.py
+++ bmstable.py
@@ -110,7 +110,7 @@ class BmsTable():
         if not 'sha256' in df_table_orig.columns:
             df_table_orig['sha256'] = ''
         df_table_orig.reset_index(inplace=True)
-        #df_table_orig.to_csv('_debug_csv/df_table_orig.csv')
+        #df_table_orig.to_csv('_debug/df_table_orig.csv')
 
         self.table_header = table_header
         self.df_table_orig = df_table_orig
@@ -148,7 +148,7 @@ class BmsTable():
         df_table['path_r2'] = df_table['path_r2'].fillna('')
         df_table['path'] = df_table['path'] + df_table['path_r2']
         df_table['found'] = df_table['path'] != ''
-        #df_table.to_csv('_debug_csv/df_table.csv')
+        #df_table.to_csv('_debug/df_table.csv')
 
         df_table.sort_values('level', key=lambda x: np.argsort(index_natsorted(df_table['level'])), inplace=True)
 
--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller --noconfirm --name bms-table-view_v1.1.2 main.py
+pyinstaller --noconfirm --clean --log-level=WARN --workpath=_build --specpath=_spec --name=bms-table-view_v1.1.2 main.py
